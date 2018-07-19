5863c69ae2f61ed9ed0ca603c363383a407cc90f
abhiram144 <venkata.abhiram144@gmail.com>
2018-07-19 09:23:00 +0530

    Fixing Some Issues

--- TravelAgent144-src Bot/Dialogs/RootLuisDialogue.cs
+++ TravelAgent144-src Bot/Dialogs/RootLuisDialogue.cs
@@ -156,22 +156,29 @@ namespace LuisBot.Dialogs
 
         public static string ToAbsoluteUrl(string relativeUrl)
         {
-            if (string.IsNullOrEmpty(relativeUrl))
+            try
+            {
+                if (string.IsNullOrEmpty(relativeUrl))
+                    return relativeUrl;
+
+                if (HttpContext.Current == null)
+                    return relativeUrl;
+
+                if (relativeUrl.StartsWith("/"))
+                    relativeUrl = relativeUrl.Insert(0, "~");
+                if (!relativeUrl.StartsWith("~/"))
+                    relativeUrl = relativeUrl.Insert(0, "~/");
+
+                var url = HttpContext.Current.Request.Url;
+                var port = url.Port != 80 ? (":" + url.Port) : String.Empty;
+
+                return String.Format("{0}://{1}{2}{3}",
+                    url.Scheme, url.Host, port, VirtualPathUtility.ToAbsolute(relativeUrl));
+            }
+            catch(Exception ex)
+            {
                 return relativeUrl;
-
-            if (HttpContext.Current == null)
-                return relativeUrl;
-
-            if (relativeUrl.StartsWith("/"))
-                relativeUrl = relativeUrl.Insert(0, "~");
-            if (!relativeUrl.StartsWith("~/"))
-                relativeUrl = relativeUrl.Insert(0, "~/");
-
-            var url = HttpContext.Current.Request.Url;
-            var port = url.Port != 80 ? (":" + url.Port) : String.Empty;
-
-            return String.Format("{0}://{1}{2}{3}",
-                url.Scheme, url.Host, port, VirtualPathUtility.ToAbsolute(relativeUrl));
+            }
         }
         private async Task ResumeAfterTravelFormDialog(IDialogContext context, IAwaitable<TravelBooking> result)
         {
